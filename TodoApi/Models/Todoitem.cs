f87a973424456bd262de825f75f8a1b4e63669e2
sirikasamkit <sirikasamkit.t@ku.th>
2026-09-13 13:39:53 +0700

    [dev] add dbcontext addd Services

---
+++ TodoApi/Models/Todoitem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models;
+
+public class Todoitem
+{
+    public int Id  { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleated { get; set; }
+    public DateTime CreatedAT { get; set; }
+}
+
